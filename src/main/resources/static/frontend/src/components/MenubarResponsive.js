0d7a0413c33805b6c5f61cbd4fd564ee2a842fd6
Javi Martínez <javimartinezfer@gmail.com>
2020-11-14 20:54:23 +0100

    Mejora de barra de navegación y restructuración de código

--- src/main/resources/static/frontend/src/components/MenubarResponsive.js
+++ src/main/resources/static/frontend/src/components/MenubarResponsive.js
@@ -4,12 +4,13 @@ import 'primereact/resources/primereact.css';
 import 'primeflex/primeflex.css';
 import '../menu.css';
 import { Component } from 'react';
-import { TabMenu } from 'primereact/tabmenu';
+import { Menubar } from 'primereact/menubar';
 
-export class MenubarDemo extends Component {
+export class MenubarResponsive extends Component {
 
     constructor(props) {
         super(props);
+
         this.state = {
             items1: [
                 {label: 'Inicio', icon: 'pi pi-fw pi-home'},
@@ -27,8 +28,14 @@ export class MenubarDemo extends Component {
     }
 
     render() {
+        const start = <img alt="logo" src="https://static.wixstatic.com/media/8d86d1_1e825f1702a2414eba98748ce4fc56b3~mv2.png/v1/fill/w_121,h_106,al_c,q_85,usm_0.66_1.00_0.01/8d86d1_1e825f1702a2414eba98748ce4fc56b3~mv2.webp" height="40" className="p-mr-2"></img>;
+
         return (
-                <TabMenu model={false ? this.state.items1:this.state.items1.concat(this.state.items2)} activeItem={this.state.activeItem} onTabChange={(e) => this.setState({activeItem: e.value})}/>
+            <div>
+                <div className="card">
+                    <Menubar model={false ? this.state.items1:this.state.items1.concat(this.state.items2)} start={start}/>
+                </div>
+            </div>
         );
     }
 }
