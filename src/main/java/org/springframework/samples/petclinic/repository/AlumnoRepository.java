4b209f7b99e0d9237348405cf7200b2d5d3bf206
almafe2510 <44414361+Almafe2510@users.noreply.github.com>
2020-11-28 14:32:30 +0100

    Update AlumnoRepository.java

--- src/main/java/org/springframework/samples/petclinic/repository/AlumnoRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/AlumnoRepository.java
@@ -14,7 +14,7 @@ public interface AlumnoRepository extends CrudRepository<Alumno, String> {
 	@Query(value = "Select * from alumnos where (alumnos.nick_usuario = :nickUsuario)", nativeQuery = true)
 	public Alumno findByNick(@Param("nickUsuario") String nickUsuario);
 	
-	@Query(value = "Select * from alumnos a natural join grupos where (grupos_nombre_grupo= :nombreGrupo)",nativeQuery = true)
+	@Query(value = "Select * from alumnos where (grupos_nombre_grupo= :nombreGrupo)",nativeQuery = true)
     public List<Alumno> findByGroup(@Param("nombreGrupo") String nombreGrupo);
 	
 	
