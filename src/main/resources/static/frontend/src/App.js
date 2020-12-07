234ae89694c0bb8d5cbc8baf18e4fc3ca39997c6
JAVIER MARTÍNEZ FERNÁNDEZ <javmarfer3@alum.us.es>
2020-12-07 14:08:06 +0100

    Navegación y mantenimiento de sesiones de usuarios completado

--- src/main/resources/static/frontend/src/App.js
+++ src/main/resources/static/frontend/src/App.js
@@ -1,43 +1,50 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Solicitudes } from './components/Solicitudes';
-import './index.css';
-import Login from './components/Login';
-import { EditStudent } from './components/EditStudent';
-import { SolicitudesProfesor } from './components/SolicitudesProfesor';
-import { AlumnosPorTutor } from './components/AlumnosPorTutor';
-import { Alumnos } from './components/Alumnos';
-import {Home} from './components/Home';
-import { MenubarResponsive } from './components/MenubarResponsive';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Solicitudes } from './components/Solicitudes'
+import './index.css'
+import Login from './components/Login'
+import { EditStudent } from './components/EditStudent'
+import { SolicitudesProfesor } from './components/SolicitudesProfesor'
+import { AlumnosPorTutor } from './components/AlumnosPorTutor'
+import { Alumnos } from './components/Alumnos'
+import {Home} from './components/Home'
+import { MenubarResponsive } from './components/MenubarResponsive'
+import {getUserType} from './components/storage'
 
 
 class App extends Component {
 
-	home = new Home();
-
 	state = {
 		urlBase: "http://localhost:8081",
-		//tipoDeUsuario: "integrante"
-		tipoDeUsuario: this.home.state.type
+		username: "",
+		userType: ""
 	}
 
-	componentDidMount(){
-		console.log(this.home.state.type);
+	changeUserType(t){
+		this.setState({userType: t})
+	}
+
+	calculateType(){
+		if(this.state.userType===""){
+			return getUserType()
+		}else{
+			return this.state.userType
+		}
 	}
 
 	render() {
 		return (
 			<div>
-			<MenubarResponsive tipoDeUsuario={this.home.state.type}></MenubarResponsive>
+			<MenubarResponsive tipoDeUsuario={this.calculateType()}></MenubarResponsive>
 				<Router>
 					<Route path="/home" render={() =>
 						<Home></Home>
 					} />
 					<Route path="/requests" render={() =>
-						<Solicitudes tipoDeUsuario={this.state.tipoDeUsuario} urlBase={this.state.urlBase}></Solicitudes>
+						<Solicitudes urlBase={this.state.urlBase}></Solicitudes>
 					} />
 					<Route path="/login" render={() =>
-						<Login urlBase={this.state.urlBase}></Login>
+						<Login urlBase={this.state.urlBase} onType={this.changeUserType.bind(this)}></Login>
 					} />
 					<Route path="/pendingRequests" render={() =>
 						<SolicitudesProfesor urlBase={this.state.urlBase}></SolicitudesProfesor>
@@ -57,4 +64,4 @@ class App extends Component {
 	}
 }
 
-export default App;
+export default App
