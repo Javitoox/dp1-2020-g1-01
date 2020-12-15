8bf42a42d0fdbd0135ab081096b19b5a0280f977
Javier Vilariño Mayo <javiervilarinyomayo.cr@gmail.com>
2020-12-15 01:49:23 +0100

    arreglos

--- src/main/java/org/springframework/samples/petclinic/repository/AlumnoRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/AlumnoRepository.java
@@ -17,13 +17,13 @@ public interface AlumnoRepository extends CrudRepository<Alumno, String> {
 	@Query(value = "Select * from alumnos where grupos_nombre_grupo= :nombreGrupo",nativeQuery = true)
     public List<Alumno> findByGroup(@Param("nombreGrupo") String nombreGrupo);
 	
-	@Query(value="SELECT * FROM ALUMNOS WHERE FECHA_SOLICITUD IS NULL",nativeQuery = true)
+	@Query(value="SELECT * FROM ALUMNOS WHERE FECHA_MATRICULACION IS NOT NULL",nativeQuery = true)
 	public List<Alumno>findStudents();
 
     @Query(value = "Select * from alumnos natural join grupos where (cursos_curso_de_ingles = :cursoDeIngles "
             + "and grupos_nombre_grupo=nombre_grupo)", nativeQuery = true)
     public List<Alumno> findStudentsByCourse(@Param("cursoDeIngles") String cursoDeIngles);
     
-    @Query(value = "SELECT * FROM ALUMNOS where(tutores_nick_usuario_tutor=:nickTutor and FECHA_SOLICITUD IS NULL)",nativeQuery = true)
+    @Query(value = "SELECT * FROM ALUMNOS where(tutores_nick_usuario_tutor=:nickTutor and FECHA_MATRICULACION IS NOT NULL)",nativeQuery = true)
     public List<Alumno> findStudentsByTutor(@Param("nickTutor")String nickTutor);
 }
--- src/main/java/org/springframework/samples/petclinic/repository/SolicitudRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/SolicitudRepository.java
@@ -8,7 +8,7 @@ import org.springframework.samples.petclinic.model.Alumno;
 
 public interface SolicitudRepository extends CrudRepository<Alumno, String> {
 	
-	@Query(value="SELECT * FROM ALUMNOS WHERE FECHA_SOLICITUD IS NOT NULL",nativeQuery = true)
+	@Query(value="SELECT * FROM ALUMNOS WHERE (FECHA_SOLICITUD IS NOT NULL and FECHA_MATRICULACION IS NULL)",nativeQuery = true)
 	public List<Alumno>findStudentsNotAcceptedYet();
 
 }
