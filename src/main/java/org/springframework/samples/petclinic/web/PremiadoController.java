0ecebb3eaccebcc96ffc07f17506ad550d3330e8
mrb <62081471+maribelrb22@users.noreply.github.com>
2020-12-17 02:07:57 +0100

    H17 backend casi lista
    Co-Authored-By: Javier Vilariño Mayo <44314228+javivm17@users.noreply.github.com>

---
+++ src/main/java/org/springframework/samples/petclinic/web/PremiadoController.java
@@ -0,0 +1,26 @@
+package org.springframework.samples.petclinic.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.model.Premiado;
+import org.springframework.samples.petclinic.service.PremiadoService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/premiados")
+@CrossOrigin("*")
+@RestController
+public class PremiadoController {
+	
+	@Autowired
+	PremiadoService premiadoService;
+	
+	@GetMapping("/{fechaWall}")
+	public List<Premiado> premiadosPorFecha(@PathVariable("fechaWall") String fechaWall){
+		return premiadoService.premiadosPorFecha(fechaWall);
+	}
+}
