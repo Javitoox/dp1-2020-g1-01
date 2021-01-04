d754e83b3f9cd1be9d68f88fc3b66996c87b6210
JAVIER MARTÍNEZ FERNÁNDEZ <javmarfer3@alum.us.es>
2021-01-04 01:59:57 +0100

    Iniciando test dobles de controladores

--- src/test/java/org/springframework/samples/petclinic/web/UsuarioControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/web/UsuarioControllerTests.java
@@ -1,22 +1,43 @@
 package org.springframework.samples.petclinic.web;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.http.MediaType;
 import org.springframework.samples.petclinic.configuration.SecurityConfiguration;
+import org.springframework.samples.petclinic.service.UsuarioService;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(controllers=UsuarioController.class,
+import static org.mockito.BDDMockito.given;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+import static org.hamcrest.Matchers.*;
+
+@WebMvcTest(controllers = UsuarioController.class,
 excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class),
 excludeAutoConfiguration= SecurityConfiguration.class)
 public class UsuarioControllerTests {
 	
-	@Autowired
-	private UsuarioController usuarioController;
+	private static final String NICK = "JaviMartinez77";
+	private static final String PASSWORD = "Testing887";
 	
 	@Autowired
 	private MockMvc mockMvc;
+	
+	@MockBean
+	private UsuarioService usuarioService;
+	
+	@Test
+	void testAuthentication() throws Exception{
+		mockMvc.perform(get("/auth").with(csrf())).andDo(print())
+		.andExpect(status().isOk());
+	}
 
 }
