d4e636f58eff0ce92d94fa71d1ed6516ca7d21ef
Gonzalo Alvarez Garcia <48485230+gonalvgar@users.noreply.github.com>
2021-01-31 23:07:27 +0100

    historia de usuario 10
    hecha a falta de complementar con el trabajo de mis compañeros

--- src/main/resources/static/frontend/src/components/NotificationProfesor.js
+++ src/main/resources/static/frontend/src/components/NotificationProfesor.js
@@ -1,57 +1,44 @@
 import React, { Component } from 'react'
-import ReactNotification from 'react-notifications-component';
-import 'react-notifications-component/dist/theme.css';
-import { store } from 'react-notifications-component';
+
 import ExtraccionSolicitudes from './ExtraccionSolicitudes';
-
+import Notificaciones from './Notificaciones';
+import axios from 'axios';
+import Auth from './Auth';
 export default class NotificationProfesor extends Component {
-
   constructor() {
     super();
     this.state = {
-      requests:  5,
-      events: 3
+      numeroSolicitudes: 0,
+      numeroEventos: 0,
+      comprobation: false
     }
     this.solicitudesComponent = new ExtraccionSolicitudes();
   }
-  async componentDidMount() {
-    this.notificaciones();
-  }
-notificaciones(){
-  store.addNotification({
-    title: "EVENTS",
-    message: "There are " + this.state.events + " new inscriptions to events that you posted",
-    type: "info",
-    insert: "top",
-    container: "top-right",
-    animationIn: ["animate__animated", "animate__fadeIn"],
-    animationOut: ["animate__animated", "animate__fadeOut"],
-    dismiss: {
-      duration: 50000,
-      onScreen: true
-    }
-  })
-  store.addNotification({
-    title: "REQUESTS",
-    message: "There are "+  this.state.requests +  " new pending requests",
-    type: "info",
-    insert: "top",
-    container: "top-right",
-    animationIn: ["animate__animated", "animate__fadeIn"],
-    animationOut: ["animate__animated", "animate__fadeOut"],
-    dismiss: {
-      duration: 50000,
-      onScreen: true
-    }
-  })
 
-}
-  render() {
+  componentDidMount() {
+    axios.get(this.props.urlBase + "/auth", {withCredentials: true}).then(res => {
+      if(res.data==="profesor"){
+          this.setState({comprobation: true})
+      }
+      })
+     this.solicitudesComponent.getSolicitudes(this.props.urlBase).then(data => this.setState({ numeroSolicitudes: data.length }));
+
+  }
+
+
+
+
+  render() {  if (!this.state.comprobation) {
+    return <Auth authority="teacher"></Auth>
+  } else {
+
+    console.log("hay en total : " + this.state.numeroSolicitudes)
     return (
-      
-      <div className="app-container">
-        <ReactNotification />
+      <div>
+        <Notificaciones solicitudes={this.state.numeroSolicitudes}></Notificaciones>
       </div>
+
     )
   }
+}
 }
