a19549c4b3a3fb3baf4af3bd03bc98d8dc195996
JAVIER MARTÍNEZ FERNÁNDEZ <javmarfer3@alum.us.es>
2021-02-01 20:47:34 +0100

    Nuevos dobles de tests de eventos. Parte de los servicios completada

--- src/test/java/org/springframework/samples/petclinic/web/EventoControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/web/EventoControllerTests.java
@@ -16,7 +16,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.List;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
