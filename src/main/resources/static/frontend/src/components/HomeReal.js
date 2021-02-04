01b3c065266832489e5a12baf955ad9640f7711a
Gonzalo Alvarez Garcia <48485230+gonalvgar@users.noreply.github.com>
2021-02-04 22:33:19 +0100

    una cosa que se me habia olvidado

--- src/main/resources/static/frontend/src/components/HomeReal.js
+++ src/main/resources/static/frontend/src/components/HomeReal.js
@@ -63,6 +63,20 @@ class HomeReal extends Component {
                 }}
                 ) 
             }
+        }else{
+            store.addNotification({
+                title: "COVID 19 INFORMATION",
+                message: "Due to the Covid-19 situation, you have the option to take online classes or to book custom presential tutorships. Get more info at https://www.mscbs.gob.es/",
+                type: "danger",
+                insert: "top",
+                container: "top-right",
+                animationIn: ["animate__animated", "animate__fadeIn"],
+                animationOut: ["animate__animated", "animate__fadeOut"],
+                dismiss: {
+                    duration: 50000,
+                    onScreen: true
+            }}
+            ) 
         }
     }
 
